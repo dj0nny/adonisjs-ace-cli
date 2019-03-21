c97074588966171bb814c62449d5126d7000c372
Dj0nny <frank96990@gmail.com>
2019-03-21 15:01:39 +0100

    add hello.js

---
+++ commands/hello.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const { Command } = require('@adonisjs/ace');
+
+class Hello extends Command {
+	static get signature() {
+		return 'hello';
+	}
+
+	static get description() {
+		return 'Say hello to the user';
+	}
+
+	async handle() {
+		console.log('Hello dude!');
+	}
+}
