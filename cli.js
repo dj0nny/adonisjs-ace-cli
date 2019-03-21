c570ec63cf598b198f2d948780f2070b98144381
Dj0nny <frank96990@gmail.com>
2019-03-21 15:56:23 +0100

    add flag command

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@ const ace = require('@adonisjs/ace');
 
 ace.addCommand(require('./commands/hello'));
 ace.addCommand(require('./commands/package'));
+ace.addCommand(require('./commands/flag'))
 
 ace.wireUpWithCommander();
 ace.invoke();
