de35ca273a03c517990f634760fac87a70debc91
Dj0nny <frank96990@gmail.com>
2019-03-22 15:57:18 +0100

    add name.js

---
+++ commands/name.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const {Command} = require('@adonisjs/ace')
+
+class Name extends Command {
+    static get signature() {
+        return `
+            name
+            { name: your name}
+        `
+    }
+
+    static get description() {
+        return 'What is your name?'
+    }
+
+    async handle({name}) {
+        console.log(`Hello ${name}`)
+    }
+}
+
+module.export = Name
