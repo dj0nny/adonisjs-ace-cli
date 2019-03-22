a2eaabe107b85ca1ac6aa10860a206562bfaa63b
Dj0nny <frank96990@gmail.com>
2019-03-22 16:01:12 +0100

    add name command

--- cli.js
+++ cli.js
@@ -4,7 +4,8 @@ const ace = require('@adonisjs/ace');
 
 ace.addCommand(require('./commands/hello'));
 ace.addCommand(require('./commands/package'));
-ace.addCommand(require('./commands/flag'))
+ace.addCommand(require('./commands/flag'));
+ace.addCommand(require('./commands/name'));
 
 ace.wireUpWithCommander();
 ace.invoke();
